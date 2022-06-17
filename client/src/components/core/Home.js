5ca6aff97df9f4d438f078eddc15dc720c18dc7e
Azalurg <91213389+Azalurg@users.noreply.github.com>
2022-06-17 22:33:40 +0200

    Update Home.js

--- client/src/components/core/Home.js
+++ client/src/components/core/Home.js
@@ -1,9 +1,37 @@
-const Home = () => {
-    return (
+import {connect } from "react-redux";
+// import { useNavigate } from 'react-router-dom';
+import React, { useEffect }  from 'react'
+import {getNotes} from "../../redux/notes/selectors"
+import { getNotesList } from '../../redux/notes/operations';
+import {getTodo} from "../../redux/todo/selectors"
+import { getTodoList } from '../../redux/todo/operations';
+
+const Home = ({notes, todo, getNotesList, getTodoList}) => {
+    // const navigate = useNavigate()
+
+    useEffect(() => {
+        getNotesList();
+        getTodoList();
+    }, [])
+
+
+    return(
         <div>
-            <h1>Home page</h1>
+            <h1>Home Page</h1>
         </div>
     )
-};
+}
 
-export default Home;
+const mapStateToProps = (state) => {
+    return {
+        notes: getNotes(state),
+        todo: getTodo(state),
+    };
+}
+
+const mapDispatchToProps = {
+    getNotesList,
+    getTodoList,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
