fbe8357c742a22fe11ed58dbf09a67a6cd9d5edd
Azalurg <91213389+Azalurg@users.noreply.github.com>
2022-06-17 22:34:06 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import TodoList from './components/todo/todoList';
+import TodoAdd from './components/todo/todoAdd';
+import Home from './components/core/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="Main">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" className="bntLink">Home</Link>
+            </li>
+            <li>
+              <Link to="/todo" className="bntLink">Todos List</Link>
+            </li>
+            <li>
+              <Link to="/todo/add" className="bntLink">Add todo</Link>
+            </li>
+            <li>
+              <Link to="/notes" className="bntLink">Note List</Link>
+            </li>
+            <li>
+              <Link to="/notes/add" className="bntLink">Add Note</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/todo/add" element={<TodoAdd/>} />
+          <Route path="/todo" element={<TodoList/>} />
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
