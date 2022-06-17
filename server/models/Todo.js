adbba83195ba910a797b74248161be48f541a045
Azalurg <91213389+Azalurg@users.noreply.github.com>
2022-06-17 22:30:54 +0200

    Update Todo Model

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -3,8 +3,8 @@ const { Schema, model, modelNames } = require("mongoose");
 const todoSchema = new Schema({
     title: {type: String, require: true},
     author: {type: String, require: true},
-    deadline: {type: Date, require: false},
-    done: {type: Boolean, require: true}
+    deadline: {type: Date, require: false, default: Date.now},
+    done: {type: Boolean, require: true, default: false}
 });
 
 module.exports = model("Todo", todoSchema)
