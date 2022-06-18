3612d5d55bc2a43c9e141898657e71b008e194f3
Azalurg <91213389+Azalurg@users.noreply.github.com>
2022-06-18 19:58:04 +0200

    new end-points
    PUT & DELETE

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -6,9 +6,7 @@ const Notes = require("../models/Notes");
 router.get("/", async (_req, res) => {
     Notes
         .find()
-        .then(ans => {
-            res.status(200).json(ans);
-        })
+        .then(ans => res.status(200).json(ans))
         .catch(err => res.status(500).json(err));
 });
 
@@ -26,6 +24,26 @@ router.post("/", async (req, res) => {
     } catch(err) {
         res.status(500).send(err)
     }
+});
+
+router.put("/:id", async (req, res) => {
+    const id = req.params.id
+
+    Notes
+        .findByIdAndUpdate(id, {...req.body}, {new: true})
+        .then(ans => res.status(201).json(ans))
+        .catch(err => res.status(500).json(err));
+});
+
+router.delete("/:id", async (req, res) => {
+    const id = req.params.id
+
+    Notes
+        .findByIdAndDelete(id)
+        .then(ans => {
+            res.status(200).json(ans);
+        })
+        .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -26,8 +26,28 @@ router.post("/", async (req, res) => {
         await new_todo.save()
         res.status(200).json(new_todo)
     } catch(err) {
-        res.status(500).send(err)
+        console.log(err.message);
+        res.status(500).json(err.message)
     }
+});
+
+router.put("/:id", async (req, res) => {
+    const id = req.params.id
+    Todo
+        .findByIdAndUpdate(id, {...req.body}, {new: true})
+        .then(ans => res.status(201).json(ans))
+        .catch(err => res.status(500).json(err));
+});
+
+router.delete("/:id", async (req, res) => {
+    const id = req.params.id
+
+    Todo
+        .findByIdAndDelete(id)
+        .then(ans => {
+            res.status(200).json(ans);
+        })
+        .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
