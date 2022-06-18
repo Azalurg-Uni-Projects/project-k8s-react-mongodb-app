bbd5ad1b712391a93946e8afa7a3856443ae8dd3
Azalurg <91213389+Azalurg@users.noreply.github.com>
2022-06-18 19:58:30 +0200

    Update todoList.js

--- client/src/components/todo/todoList.js
+++ client/src/components/todo/todoList.js
@@ -1,17 +1,22 @@
 import { useDispatch, useSelector, connect } from "react-redux";
-// import { deleteTodoAction, finishTodoAction } from '../../redux/actions/todoActions'
 import {getTodo} from "../../redux/todo/selectors"
 import { Link } from "react-router-dom";
-import { getTodoList } from '../../redux/todo/operations';
+import { getTodoList, deleteTodo, editTodo } from '../../redux/todo/operations';
 import React, { useEffect }  from 'react'
 
-const TodoList = ({todo, getTodoList}) => {
+const TodoList = ({todo, getTodoList, deleteTodo, editTodo}) => {
     const dispatch = useDispatch();
 
     const done = (item) =>{
         if (!item.done) return(<div style={{color: "red"}}>Done: Todo</div>);
         else return(<div style={{color: "green"}}>Done: Finished</div>)
     }
+
+    const doIt = (item) => {
+        item.done = !item.done;
+        editTodo(item)
+    }
+
 
     useEffect(() => {
         getTodoList();
@@ -20,13 +25,15 @@ const TodoList = ({todo, getTodoList}) => {
     return (
         <div>
             <h1>Todo List</h1>
-            <div>
+            <div className="List">
                 {todo.map(item => (
                     <div key={item._id}>
                         <h3>{item.title}</h3>
                         Author: {item.author}<br/>
                         Deadline: {new Date(item.deadline).toLocaleDateString('pl-PL')}
                         {done(item)}
+                        <button className="Bnt " onClick={() => doIt(item)}>Do</button>
+                        <button className="Bnt Delete" onClick={() => deleteTodo(item)}>Delete</button>
                     </div>
                 ))}
             </div>
@@ -42,7 +49,9 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = {
-    getTodoList
+    getTodoList,
+    deleteTodo,
+    editTodo
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
